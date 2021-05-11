4920069a414950a6958f0043b29ae56956b0bbc8
me <me@me.com>
2021-05-11 09:40:48 +0000

    Fleshed out some show_time.c

--- time/src/show_time.c
+++ time/src/show_time.c
@@ -1,9 +1,19 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <time.h>
 
 int main(int argc, char *argv[]) {
     time_t time_sec = time(NULL);
+
+    char *timeStr = ctime(&time_sec);
+    if (timeStr == NULL) {
+        
+        exit(EXIT_FAILURE);
+    }
+    printf("ctime() of time() value is: %s\n"
 
     return 0;
 }
